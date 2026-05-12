039c2ed7351cb243ca18f63f30a17eeefcb87aaa
Oktora15 <nitoruciane@gmail.com>
2026-05-12 17:07:19 +0700

    add CI workflow and modelling script

---
+++ modelling.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# ======================
+# SET EXPERIMENT
+# ======================
+mlflow.set_experiment("Heart_Disease_Experiment")
+
+# ======================
+# LOAD DATA
+# ======================
+df = pd.read_csv("dataset_preprocessing.csv")
+
+X = df.drop("target", axis=1)
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# ======================
+# TRAINING + LOGGING
+# ======================
+with mlflow.start_run():
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+
+    print("Accuracy:", acc)
+
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("random_state", 42)
+    mlflow.log_metric("accuracy", acc)
+
+    mlflow.sklearn.log_model(model, "model")
